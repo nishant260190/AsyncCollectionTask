423e664846b49d2576c2f5765aa4c775d3a99722
prashant <prashantshri007@gmail.com>
2017-03-27 10:41:38 +0530

    Initial Commit

---
+++ AsyncCollectionTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AsyncCollectionTask
+//
+//  Created by Roposo on 3/27/17.
+//  Copyright © 2017 Roposo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AsyncCollectionTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AsyncCollectionTask
+//
+//  Created by Roposo on 3/27/17.
+//  Copyright © 2017 Roposo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
